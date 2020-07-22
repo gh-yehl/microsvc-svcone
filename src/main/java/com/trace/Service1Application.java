086c767c0c2291886bb504af04b4e873194ed883
gh-yehl <54838149+gh-yehl@users.noreply.github.com>
2020-07-22 15:09:33 +0800

    update

---
+++ src/main/java/com/trace/Service1Application.java
@@ -0,0 +1,43 @@
+package com.trace;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+public class Service1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Service1Application.class, args);
+    }
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Bean
+    RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @RequestMapping("/call1")
+    public String callService1(){
+        return "Service1 called...";
+    }
+
+    @RequestMapping("/call2")
+    public String callService2(){
+        System.out.println("Make call2...");
+        return restTemplate.getForObject("http://localhost:8772/call2", String.class);
+    }
+
+    @RequestMapping("/call3")
+    public String callService3(){
+        System.out.println("Make call3 ...");
+        return restTemplate.getForObject("http://localhost:8772/call3", String.class);
+    }
+}
